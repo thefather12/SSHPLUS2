dabf39857f02f59b076dab793f4089277d9748ed
thefather12 <94747184+thefather12@users.noreply.github.com>
2023-09-18 21:01:10 -0600

    Update botteste.sh

--- Sistema/botteste.sh
+++ Sistema/botteste.sh
@@ -10,7 +10,7 @@
     echo -e "\033[1;32mINICIANDO BOT DE PRUEVAS \033[0m\n"
     cd $HOME/BOT
     rm -rf $HOME/BOT/botssh
-    wget https://raw.githubusercontent.com/thefather12/SSHPLUS/main/Sistema/botssh >/dev/null 2>&1
+    wget https://raw.githubusercontent.com/thefather12/SSHPLUS2/main/Sistema/botssh >/dev/null 2>&1
     chmod 777 botssh
     echo ""
     sleep 1
